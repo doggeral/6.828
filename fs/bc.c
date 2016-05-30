bd025d2340a8a56f532f63368a7270f531987613
longfang <zhaolongfang1@163.com>
2016-05-30 23:18:14 +0800

    Lab5 ex2.

--- fs/bc.c
+++ fs/bc.c
@@ -48,6 +48,12 @@ bc_pgfault(struct UTrapframe *utf)
 	// the disk.
 	//
 	// LAB 5: you code here:
+	addr = ROUNDDOWN(addr, PGSIZE);
+	if ((r = sys_page_alloc (0, addr, PTE_U|PTE_P|PTE_W)) < 0) {
+		panic ("fs page allocation error : %e", r);
+	}
+	//Notice: It's sector instead of block
+	ide_read(blockno * BLKSECTS, addr, BLKSECTS);
 
 	// Clear the dirty bit for the disk block page since we just read the
 	// block from disk
@@ -73,12 +79,25 @@ void
 flush_block(void *addr)
 {
 	uint32_t blockno = ((uint32_t)addr - DISKMAP) / BLKSIZE;
+	int r;
 
 	if (addr < (void*)DISKMAP || addr >= (void*)(DISKMAP + DISKSIZE))
 		panic("flush_block of bad va %08x", addr);
 
 	// LAB 5: Your code here.
-	panic("flush_block not implemented");
+	//panic("flush_block not implemented");
+	addr = ROUNDDOWN(addr, PGSIZE);
+	if (!va_is_mapped(addr) || !va_is_dirty(addr)) {
+		return;
+	}
+
+	ide_write(blockno * BLKSECTS, addr, BLKSECTS);
+
+	if ((r = sys_page_map(0, addr, 0, addr, uvpt[PGNUM(addr)] & PTE_SYSCALL)) < 0) {
+		panic("in flush_block, sys_page_map: %e", r);
+	}
+
+	return;
 }
 
 // Test that the block cache works, by smashing the superblock and
